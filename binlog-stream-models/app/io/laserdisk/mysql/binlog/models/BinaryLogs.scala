0c47f5461427ab7e4c11b19baf5b3624e66885f5
d.semenov <dmytro.semenov@modaoperandi.com>
2020-05-22 16:56:12 -0400

    formatting

--- binlog-stream-models/app/io/laserdisk/mysql/binlog/models/BinaryLogs.scala
+++ binlog-stream-models/app/io/laserdisk/mysql/binlog/models/BinaryLogs.scala
@@ -8,12 +8,12 @@ import doobie.implicits._
 case class BinaryLogs(fileName: String, size: Long)
 
 object BinaryLogs {
-  def showLogs[F[_]](implicit xa: Transactor[F],
-                     ev: Bracket[F, Throwable]): F[List[BinaryLogs]] =
+  def showLogs[F[_]](implicit xa: Transactor[F], ev: Bracket[F, Throwable]): F[List[BinaryLogs]] =
     sql"""show binary logs""".query[BinaryLogs].to[List].transact(xa)
 
   def bytesBehindTheHead[F[_]: Transactor](fileName: String, pos: Long)(
-      implicit ev: Bracket[F, Throwable]): F[Long] =
+    implicit ev: Bracket[F, Throwable]
+  ): F[Long] =
     showLogs[F].map { logs =>
       logs.dropWhile(log => log.fileName != fileName).foldLeft(0L) {
         case (sum, l) => sum + l.size
