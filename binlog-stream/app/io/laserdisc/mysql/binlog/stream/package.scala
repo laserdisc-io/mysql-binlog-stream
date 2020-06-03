13d54dd85ece3360ac49ec23e0c6986bb905aedf
d.semenov <dmytro.semenov@modaoperandi.com>
2020-06-03 14:41:46 -0400

    formatting

--- binlog-stream/app/io/laserdisc/mysql/binlog/stream/package.scala
+++ binlog-stream/app/io/laserdisc/mysql/binlog/stream/package.scala
@@ -1,7 +1,7 @@
 package io.laserdisc.mysql.binlog
 
 import cats.effect.concurrent.Ref
-import cats.effect.{ ConcurrentEffect, Sync }
+import cats.effect.{ConcurrentEffect, Sync}
 import cats.implicits._
 import com.github.shyiko.mysql.binlog.event.Event
 import event.EventMessage
@@ -9,24 +9,27 @@ import io.chrisdavenport.log4cats.SelfAwareStructuredLogger
 
 package object stream {
   def streamEvents[F[_]: ConcurrentEffect: SelfAwareStructuredLogger](
-    transactionState: Ref[F, TransactionState]
+      transactionState: Ref[F, TransactionState]
   ): fs2.Pipe[F, Event, EventMessage] =
-    _.evalMap(event =>
-      SelfAwareStructuredLogger[F]
-        .debug(s"received binlog event $event") >> transactionState
-        .modifyState(TransactionState.nextState(event))
-    ).unNone
-      .flatMap(pkg => fs2.Stream.eval(warnBigTransactionPackage(pkg)) *> fs2.Stream(pkg.events: _*))
+    _.evalMap(
+      event =>
+        SelfAwareStructuredLogger[F]
+          .debug(s"received binlog event $event") >> transactionState
+          .modifyState(TransactionState.nextState(event))).unNone
+      .flatMap(pkg =>
+        fs2.Stream.eval(warnBigTransactionPackage(pkg)) >> fs2.Stream(
+          pkg.events: _*))
 
   def warnBigTransactionPackage[F[_]: Sync: SelfAwareStructuredLogger](
-    transactionPackage: TransactionPackage
+      transactionPackage: TransactionPackage
   ): F[Unit] =
     if (transactionPackage.events.size >= 1000)
       for {
         distro <- Sync[F].delay(transactionPackage.events.groupBy(_.table).map {
-                    case (k, v) => k -> v.size
-                  })
-        _ <- SelfAwareStructuredLogger[F].warn(s"""Transaction has > then 1000 elements in it with
+          case (k, v) => k -> v.size
+        })
+        _ <- SelfAwareStructuredLogger[F].warn(
+          s"""Transaction has > then 1000 elements in it with
                                  |following distribution $distro
         """.stripMargin)
       } yield ()
