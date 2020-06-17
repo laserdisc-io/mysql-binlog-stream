a15ae17377a208fed73d7eff656c95a1f5261eab
d.semenov <dmytro.semenov@modaoperandi.com>
2020-06-17 16:27:15 -0400

    use table and pk to uniquely identify the record for compaction

--- binlog-stream/app/io/laserdisc/mysql/binlog/compaction/package.scala
+++ binlog-stream/app/io/laserdisc/mysql/binlog/compaction/package.scala
@@ -6,22 +6,26 @@ import io.laserdisc.mysql.binlog.event.EventMessage
 
 import scala.collection.mutable
 import cats.implicits._
+import io.laserdisc.mysql.json
 
 package object compaction {
   def compact(transaction: Seq[EventMessage]): Seq[EventMessage] =
     transaction
-      .foldLeft(mutable.LinkedHashMap.empty[Json, EventMessage]) {
+      .foldLeft(mutable.LinkedHashMap.empty[String, EventMessage]) {
         case (acc, evt) =>
-          acc.get(evt.pk).fold[Unit](acc.put(evt.pk, evt)) { latest =>
-            mkNewEvent(evt)
-              .andThen(finalizeNewEvent)
-              .run(latest) match {
-              case Some(ne) =>
-                acc.remove(evt.pk);
-                acc.put(evt.pk, ne)
-              case None => acc.remove(evt.pk)
+          val emId = calcIdentity(evt)
+          acc
+            .get(emId)
+            .fold[Unit](acc.put(emId, evt)) { latest =>
+              mkNewEvent(evt)
+                .andThen(finalizeNewEvent)
+                .run(latest) match {
+                case Some(ne) =>
+                  acc.remove(emId);
+                  acc.put(emId, ne)
+                case None => acc.remove(emId)
+              }
             }
-          }
           //TODO: use this code, once Scala 2.12 support ends, or  updateWith back ported to 2.12A
 //          acc.updateWith(evt.pk) {
 //            case Some(latest) =>
@@ -60,4 +64,7 @@ package object compaction {
       }
       .flatten
   )
+
+  def calcIdentity(em: EventMessage): String =
+    (em.table +: json.flatSort(em.pk)).mkString("|")
 }
