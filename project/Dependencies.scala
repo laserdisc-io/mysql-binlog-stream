ca8679c8a8a58c5b6f753da1eee9aa13b9baf54c
Scala Steward <me@scala-steward.org>
2020-07-17 14:42:06 +0200

    Update mockito-core to 3.4.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Dependencies {
       "org.scalamock"     %% "scalamock"            % "4.4.0"       % Test,
       "com.dimafeng"      %% "testcontainers-scala" % "0.36.1"      % Test,
       "org.testcontainers" % "mysql"                % "1.13.0"      % Test,
-      "org.mockito"        % "mockito-core"         % "3.3.3"       % Test,
+      "org.mockito"        % "mockito-core"         % "3.4.3"       % Test,
       "io.laserdisc"      %% "fs2-aws-testkit"      % fs2AwsVersion % Test,
       "com.dimafeng"      %% "testcontainers-scala" % "0.35.0"      % Test
     )
