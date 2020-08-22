4850ec2a2df52495a02039d48706092d52163a88
Scala Steward <me@scala-steward.org>
2020-08-22 07:23:55 +0200

    Update scalatest to 3.2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     libraryDependencies ++= Seq(
       "io.github.sullis"  %% "jms-testkit"          % "0.2.8"       % Test, // ApacheV2
       "org.scalamock"     %% "scalamock"            % "5.0.0"       % Test,
-      "org.scalatest"     %% "scalatest"            % "3.2.1"       % Test, // ApacheV2
+      "org.scalatest"     %% "scalatest"            % "3.2.2"       % Test, // ApacheV2
       "com.dimafeng"      %% "testcontainers-scala" % "0.38.1"      % Test,
       "org.testcontainers" % "mysql"                % "1.14.3"      % Test,
       "org.mockito"        % "mockito-core"         % "3.4.6"       % Test,
