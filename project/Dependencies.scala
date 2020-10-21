d98bb4e3a464dac1e533f9a8cc4e19f59f9dfc86
Scala Steward <me@scala-steward.org>
2020-10-21 06:35:51 +0200

    Update mysql-connector-java to 8.0.22

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -84,7 +84,7 @@ object Dependencies {
       "org.tpolecat"      %% "doobie-refined"              % doobieVersion, // Postgres driver 42.1.4 + type mappings.
       "org.tpolecat"      %% "doobie-scalatest"            % doobieVersion, // Support for doobie scalatest
       "com.googlecode.usc" % "jdbcdslog"                   % jdbcdslogVersion,
-      "mysql"              % "mysql-connector-java"        % "8.0.21",
+      "mysql"              % "mysql-connector-java"        % "8.0.22",
       "com.github.shyiko"  % "mysql-binlog-connector-java" % "0.21.0"
     )
   )
