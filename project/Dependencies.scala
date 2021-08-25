8d935d0acece21425aecfcb99e56e71719ad3758
Barry O'Neill <barra.oneill@gmail.com>
2021-08-25 13:18:59 -0400

    Move to zendesk mysql connector

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,15 +5,15 @@ object Dependencies {
   val cirisVersion  = "1.2.1"
   val doobieVersion = "0.13.4"
   val circeVersion  = "0.14.1"
-  val catsVersion   = "2.5.1"
+  val catsVersion   = "2.5.3"
 
   val TestLib = Seq(
     libraryDependencies ++= Seq(
       "org.scalamock"     %% "scalamock"            % "5.1.0"  % Test,
       "org.scalatest"     %% "scalatest"            % "3.2.9"  % Test,
-      "com.dimafeng"      %% "testcontainers-scala" % "0.39.5" % Test,
-      "org.testcontainers" % "mysql"                % "1.15.3" % Test,
-      "org.testcontainers" % "testcontainers"       % "1.15.3" % Test
+      "com.dimafeng"      %% "testcontainers-scala" % "0.39.6" % Test,
+      "org.testcontainers" % "mysql"                % "1.16.0" % Test,
+      "org.testcontainers" % "testcontainers"       % "1.16.0" % Test
     )
   )
 
@@ -21,28 +21,28 @@ object Dependencies {
     libraryDependencies ++= Seq(
       "is.cir"     %% "ciris-enumeratum" % cirisVersion,
       "is.cir"     %% "ciris-refined"    % cirisVersion,
-      "eu.timepit" %% "refined"          % "0.9.26"
+      "eu.timepit" %% "refined"          % "0.9.27"
     )
   )
 
   val Logging = Seq(
     libraryDependencies ++= Seq(
-      "ch.qos.logback" % "logback-classic" % "1.2.3", // logging
-      "ch.qos.logback" % "logback-core"    % "1.2.3", // logging
-      "org.slf4j"      % "jcl-over-slf4j"  % "1.7.31",
-      "org.slf4j"      % "jul-to-slf4j"    % "1.7.31",
+      "ch.qos.logback" % "logback-classic" % "1.2.5", // logging
+      "ch.qos.logback" % "logback-core"    % "1.2.5", // logging
+      "org.slf4j"      % "jcl-over-slf4j"  % "1.7.32",
+      "org.slf4j"      % "jul-to-slf4j"    % "1.7.32",
       "org.typelevel" %% "log4cats-slf4j"  % "1.3.1"
     )
   )
 
   val Persistence = Seq(
     libraryDependencies ++= Seq(
-      "org.tpolecat"     %% "doobie-core"                 % doobieVersion,
-      "org.tpolecat"     %% "doobie-hikari"               % doobieVersion, // HikariCP transactor.
-      "org.tpolecat"     %% "doobie-refined"              % doobieVersion, // Postgres driver 42.1.4 + type mappings.
-      "org.tpolecat"     %% "doobie-scalatest"            % doobieVersion % Test, // Support for doobie scalatest
-      "mysql"             % "mysql-connector-java"        % "8.0.25",
-      "com.github.shyiko" % "mysql-binlog-connector-java" % "0.21.0"
+      "org.tpolecat" %% "doobie-core"                 % doobieVersion,
+      "org.tpolecat" %% "doobie-hikari"               % doobieVersion, // HikariCP transactor.
+      "org.tpolecat" %% "doobie-refined"              % doobieVersion, // Postgres driver 42.1.4 + type mappings.
+      "org.tpolecat" %% "doobie-scalatest"            % doobieVersion % Test, // Support for doobie scalatest
+      "mysql"         % "mysql-connector-java"        % "8.0.26",
+      "com.zendesk"   % "mysql-binlog-connector-java" % "0.25.3"
     )
   )
 
@@ -64,8 +64,8 @@ object Dependencies {
   val XML = Seq(
     libraryDependencies ++= Seq(
       "javax.xml.bind"   % "jaxb-api"   % "2.3.1",
-      "com.sun.xml.bind" % "jaxb-impl"  % "3.0.1",
-      "com.sun.xml.bind" % "jaxb-core"  % "3.0.1",
+      "com.sun.xml.bind" % "jaxb-impl"  % "3.0.2",
+      "com.sun.xml.bind" % "jaxb-core"  % "3.0.2",
       "javax.activation" % "activation" % "1.1.1"
     )
   )
