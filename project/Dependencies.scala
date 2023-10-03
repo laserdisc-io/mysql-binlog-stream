62ebb9520c90d0da0eb1654bc5559432e498b555
Scala Steward <scala_steward@virtuslab.com>
2023-10-03 22:04:42 +0000

    Update mysql-connector-java to 8.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -41,7 +41,7 @@ object Dependencies {
       "org.tpolecat" %% "doobie-hikari"               % doobieVersion, // HikariCP transactor.
       "org.tpolecat" %% "doobie-refined"              % doobieVersion, // Postgres driver 42.1.4 + type mappings.
       "org.tpolecat" %% "doobie-scalatest"            % doobieVersion % Test, // Support for doobie scalatest
-      "mysql"         % "mysql-connector-java"        % "8.0.33",
+      "com.mysql"     % "mysql-connector-j"           % "8.1.0",
       "com.zendesk"   % "mysql-binlog-connector-java" % "0.28.1"
     )
   )
