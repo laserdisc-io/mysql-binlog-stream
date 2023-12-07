414a53405a618a83d840f92145950f381f8251e0
Scala Steward <scala_steward@virtuslab.com>
2023-12-07 16:38:12 +0000

    Update doobie-core, doobie-hikari, ... to 1.0.0-RC5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt.Keys.libraryDependencies
 
 object Dependencies {
   val cirisVersion      = "3.3.0"
-  val doobieVersion     = "1.0.0-RC4"
+  val doobieVersion     = "1.0.0-RC5"
   val circeVersion      = "0.14.6"
   val catsEffectVersion = "3.5.2"
 
