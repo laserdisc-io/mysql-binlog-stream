7f99304e36b9010ed9b3ca82828912bfcafb4184
Scala Steward <scala_steward@virtuslab.com>
2024-10-30 20:13:15 +0000

    Update mysql, testcontainers to 1.20.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,8 +12,8 @@ object Dependencies {
       "org.scalamock"     %% "scalamock"            % "6.0.0"  % Test,
       "org.scalatest"     %% "scalatest"            % "3.2.19" % Test,
       "com.dimafeng"      %% "testcontainers-scala" % "0.41.4" % Test,
-      "org.testcontainers" % "mysql"                % "1.20.2" % Test,
-      "org.testcontainers" % "testcontainers"       % "1.20.2" % Test
+      "org.testcontainers" % "mysql"                % "1.20.3" % Test,
+      "org.testcontainers" % "testcontainers"       % "1.20.3" % Test
     )
   )
 
