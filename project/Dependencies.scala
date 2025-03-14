ebc90d82d74a6870e3b27e126fe44a9f43082ea7
Barry O'Neill <barra.oneill@gmail.com>
2025-03-14 15:36:36 -0400

    upgrades

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,29 +2,29 @@ import sbt._
 import sbt.Keys.libraryDependencies
 
 object Dependencies {
-  val cirisVersion  = "3.6.0"
-  val doobieVersion = "1.0.0-RC6"
+  val cirisVersion  = "3.7.0"
+  val doobieVersion = "1.0.0-RC8"
   val circeVersion  = "0.14.10"
 
   val TestLib = libraryDependencies ++= Seq(
-    "org.scalamock"     %% "scalamock"            % "6.0.0"  % Test,
+    "org.scalamock"     %% "scalamock"            % "6.2.0"  % Test,
     "org.scalatest"     %% "scalatest"            % "3.2.19" % Test,
-    "com.dimafeng"      %% "testcontainers-scala" % "0.41.4" % Test,
-    "org.testcontainers" % "mysql"                % "1.20.4" % Test,
-    "org.testcontainers" % "testcontainers"       % "1.20.4" % Test
+    "com.dimafeng"      %% "testcontainers-scala" % "0.43.0" % Test,
+    "org.testcontainers" % "mysql"                % "1.20.6" % Test,
+    "org.testcontainers" % "testcontainers"       % "1.20.6" % Test
   )
 
   val Config = libraryDependencies ++= Seq(
     "is.cir"     %% "ciris-enumeratum" % cirisVersion,
     "is.cir"     %% "ciris-refined"    % cirisVersion,
-    "eu.timepit" %% "refined"          % "0.11.2"
+    "eu.timepit" %% "refined"          % "0.11.3"
   )
 
   val Logging = libraryDependencies ++= Seq(
-    "ch.qos.logback" % "logback-classic" % "1.5.12",
-    "ch.qos.logback" % "logback-core"    % "1.5.12",
-    "org.slf4j"      % "jcl-over-slf4j"  % "2.0.16",
-    "org.slf4j"      % "jul-to-slf4j"    % "2.0.16",
+    "ch.qos.logback" % "logback-classic" % "1.5.17",
+    "ch.qos.logback" % "logback-core"    % "1.5.17",
+    "org.slf4j"      % "jcl-over-slf4j"  % "2.0.17",
+    "org.slf4j"      % "jul-to-slf4j"    % "2.0.17",
     "org.typelevel" %% "log4cats-slf4j"  % "2.7.0"
   )
 
@@ -42,8 +42,6 @@ object Dependencies {
     "io.circe" %% "circe-parser" % circeVersion,
     "io.circe" %% "circe-optics" % "0.15.0" % Test
   )
-
-//  val Cats = libraryDependencies ++= Seq("org.typelevel" %% "cats-effect" % "3.5.6")
 
   val XML = libraryDependencies ++= Seq(
     "javax.xml.bind"   % "jaxb-api"   % "2.3.1",
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
 addSbtPlugin("org.scoverage"  % "sbt-coveralls"          % "1.3.11")
 addSbtPlugin("org.scoverage"  % "sbt-scoverage"          % "2.2.2")
-addSbtPlugin("io.laserdisc"   % "sbt-laserdisc-defaults" % "0.2.0")
+addSbtPlugin("io.laserdisc"   % "sbt-laserdisc-defaults" % "0.3.0")
 addSbtPlugin("com.github.sbt" % "sbt-ci-release"         % "1.9.0")
