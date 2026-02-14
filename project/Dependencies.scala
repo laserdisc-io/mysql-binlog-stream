4fa46067782d566ce1de5a6fca340886f6c6f864
Scala Steward <scala_steward@virtuslab.com>
2026-02-14 17:40:07 +0000

    Update mysql-connector-j to 9.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -33,7 +33,7 @@ object Dependencies {
     "org.tpolecat" %% "doobie-hikari"               % doobieVersion,
     "org.tpolecat" %% "doobie-refined"              % doobieVersion,
     "org.tpolecat" %% "doobie-scalatest"            % doobieVersion % Test,
-    "com.mysql"     % "mysql-connector-j"           % "9.4.0",
+    "com.mysql"     % "mysql-connector-j"           % "9.6.0",
     "com.zendesk"   % "mysql-binlog-connector-java" % "0.30.1"
   )
 
