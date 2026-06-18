a9d791fb967698d568dba701f75bdb514622db7a
Scala Steward <scala_steward@virtuslab.com>
2026-06-18 17:29:12 +0000

    Update jaxb-core, jaxb-impl to 4.0.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -45,8 +45,8 @@ object Dependencies {
 
   val XML = libraryDependencies ++= Seq(
     "javax.xml.bind"   % "jaxb-api"   % "2.3.1",
-    "com.sun.xml.bind" % "jaxb-impl"  % "4.0.6",
-    "com.sun.xml.bind" % "jaxb-core"  % "4.0.6",
+    "com.sun.xml.bind" % "jaxb-impl"  % "4.0.9",
+    "com.sun.xml.bind" % "jaxb-core"  % "4.0.9",
     "javax.activation" % "activation" % "1.1.1"
   )
 
