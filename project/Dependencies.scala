4497927fe50e6bb663dc4e3ae867981abc7c5caa
Scala Steward <scala_steward@virtuslab.com>
2026-07-18 19:00:05 +0000

    Update mysql-binlog-connector-java to 0.31.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -34,7 +34,7 @@ object Dependencies {
     "org.tpolecat" %% "doobie-refined"              % doobieVersion,
     "org.tpolecat" %% "doobie-scalatest"            % doobieVersion % Test,
     "com.mysql"     % "mysql-connector-j"           % "9.7.0",
-    "com.zendesk"   % "mysql-binlog-connector-java" % "0.30.3"
+    "com.zendesk"   % "mysql-binlog-connector-java" % "0.31.0"
   )
 
   val Circe = libraryDependencies ++= Seq(
